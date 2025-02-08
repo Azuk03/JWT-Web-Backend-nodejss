395b427d8249e54096c22be4ae197bd8e83695cf
Azuk03 <meocuptai2@gmail.com>
2025-02-08 14:16:00 +0700

    first commit

---
+++ src/routes/web.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+const initWebRoutes = (app) => {
+    router.get("/",(req,res) => {
+        res.send("Hello");
+    })
+
+    return app.use("/", router);
+}
+
+export default initWebRoutes;
